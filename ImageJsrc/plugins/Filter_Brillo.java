a6d70439e7e339df09ca6a488a6576ce08f3effb
Sebastian Murillo <sebasty06@outlook.com>
2024-08-25 19:22:49 -0500

    Add changes

---
+++ ImageJsrc/plugins/Filter_Brillo.java
@@ -0,0 +1,40 @@
+import ij.*;
+import ij.process.*;
+import ij.gui.*;
+import java.awt.*;
+import ij.plugin.filter.*;
+
+public class Filter_Brillo implements PlugInFilter {
+    ImagePlus imp;
+
+    @Override
+    public int setup(String arg, ImagePlus imp) {
+        this.imp = imp;
+        return DOES_ALL;
+    }
+
+    @Override
+    public void run(ImageProcessor ip) {
+        int height = ip.getHeight();
+        int width = ip.getWidth();
+        int pixel;
+        byte[] pixels = (byte[])ip.getPixels();
+        
+        for(int i=0; i<(height*width); i++){
+            /* pixel = (pixels[i] & 0xff) - 50;
+            if(pixel > 255) {
+                pixels[i]=(byte)(255);
+            } else if(pixel < 0) {
+                pixels[i]=(byte)(0);
+            } else {
+                pixels[i] = (byte)pixel;
+            }*/
+            
+            pixel = (pixels[i] & 0xff) - 50;
+            pixels[i] = (byte) ((pixel>255)? 255: (pixel<0)? 0: pixel);
+            
+            imp.updateAndDraw();
+        }
+    }
+
+}
---
+++ ImageJsrc/plugins/Filter_Gaussiano.java
@@ -0,0 +1,40 @@
+import ij.*;
+import ij.process.*;
+import ij.gui.*;
+import java.awt.*;
+import ij.plugin.filter.*;
+import java.util.Random;
+
+public class Filter_Gaussiano implements PlugInFilter {
+    ImagePlus imp;
+
+    @Override
+    public int setup(String arg, ImagePlus imp) {
+        this.imp = imp;
+        return DOES_8G; 
+    }
+
+    @Override
+    public void run(ImageProcessor ip) {
+        int height = ip.getHeight();
+        int width = ip.getWidth();
+        byte[] pixels = (byte[])ip.getPixels();
+
+        Random random = new Random();
+        double media = 0.0;
+        double desviacion = 20.0; 
+
+        for(int i=0; i<(height*width); i++){
+         
+            double ruido = media + desviacion * random.nextGaussian();
+            int ruidoValor = (int)((pixels[i] & 0xff) + ruido);
+            
+            
+            ruidoValor = Math.min(255, Math.max(0, ruidoValor));
+            
+            pixels[i] = (byte)ruidoValor;
+        }
+
+        imp.updateAndDraw(); 
+    }
+}
