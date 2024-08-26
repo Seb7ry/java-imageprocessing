b77281615d7fb7a6308f9ce497e62d9799d491ba
Sebastian Murillo <sebasty06@outlook.com>
2024-08-25 19:14:55 -0500

    Filters created (negative, bright and gaussian sound)

---
+++ ImageJsrc/plugins/Filter_Negative2.java
@@ -0,0 +1,28 @@
+import ij.*;
+import ij.process.*;
+import ij.gui.*;
+import java.awt.*;
+import ij.plugin.filter.*;
+
+public class Filter_Negative2 implements PlugInFilter {
+    ImagePlus imp;
+
+    @Override
+    public int setup(String arg, ImagePlus imp) {
+        this.imp = imp;
+        return DOES_8G; //Se indica para que solo se ejecute bajo imágenes en grises.
+    }
+
+    @Override
+    public void run(ImageProcessor ip) {
+        int height = ip.getHeight();
+        int width = ip.getWidth();
+        byte[] pixels = (byte[])ip.getPixels();
+        
+        for(int i=0; i<(height*width); i++){
+            pixels[i]=(byte)(255-pixels[i]);
+            imp.updateAndDraw();
+        }
+    }
+
+}
